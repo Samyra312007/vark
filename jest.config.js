06855b66aa754f0c47a024e0f9d934cafdf80ee5
Samyra312007 <samayra312007@gmail.com>
2026-07-02 20:45:52 +0530

    feat: jest config inclusion

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  testMatch: ["**/*.test.ts"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
